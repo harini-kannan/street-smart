99658cb850f651df4020be6699eae0b405224ecd
Harini Kannan <hkannan@mit.edu>
2014-08-02 15:48:33 -0700

    Added Crime API functionality

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var http = require("http");
 var Firebase = require('firebase')
-var myRootRef = new Firebase('streetsmartdb.firebaseIO.com')
+var myRootRef = new Firebase('streetsmartdb.firebaseIO.com/crimes')
 myRootRef.set("bebe")
+
+
+var options = {
+  host: 'sanfrancisco.crimespotting.org',
+  path: '/crime-data?format=tsv&count=1'
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
 
 function start() {
   function onRequest(request, response) {
@@ -12,6 +32,7 @@ function start() {
   }
 
   http.createServer(onRequest).listen(8888);
+  http.request(options, callback).end();
   console.log("Server has started.");
 }
 
