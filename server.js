da1ebaa631b68dc34d39d1b27fe469df7d08f434
Harini Kannan <hkannan@mit.edu>
2014-08-02 16:19:00 -0700

    Added Crime API functionality

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ var options = {
 
 callback = function(response) {
   var str = '';
+  console.log(response)
 
-  //another chunk of data has been recieved, so append it to `str`
+/*
   response.on('data', function (chunk) {
     str += chunk;
   });
 
-  //the whole response has been recieved, so we just print it out here
   response.on('end', function () {
     console.log(str);
-  });
+  }); */
 }
 
 function start() {
