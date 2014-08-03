2eb8b49134362dd84833641e52e33fae5d0e6028
Harini Kannan <hkannan@mit.edu>
2014-08-02 17:15:34 -0700

    Pushes crime info to Firebase

--- server.js
+++ server.js
@@ -2,32 +2,39 @@ var http = require("http");
 var Firebase = require('firebase')
 var myRootRef = new Firebase('streetsmartdb.firebaseIO.com/crimes')
 myRootRef.set("bebe")
-
+var jsonObject;
 
 var options = {
   host: 'sanfrancisco.crimespotting.org',
-  path: '/crime-data?format=tsv&count=1'
+  path: '/crime-data?format=json&count=2'
 };
 
 callback = function(response) {
   var str = '';
-  console.log(response)
 
-/*
+  // note: I think you can get address as well. look into it later
+
   response.on('data', function (chunk) {
-    str += chunk;
+    jsonObject = JSON.parse(chunk);
+    var i=0;
+    for (i=0; i < jsonObject["features"].length; i++) {
+      coordinates = jsonObject["features"][i]["geometry"]["coordinates"]
+      crimeType = jsonObject["features"][i]["properties"]["crime_type"]
+      dateTime = jsonObject["features"][i]["properties"]["date_time"]
+      description = jsonObject["features"][i]["properties"]["description"]
+      crime = {"coordinates" : coordinates, "crimeType" : crimeType, "dateTime" : dateTime, "description" : description}
+
+      myRootRef.push(crime)
+    }
   });
 
-  response.on('end', function () {
-    console.log(str);
-  }); */
 }
 
 function start() {
   function onRequest(request, response) {
-    console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World moo");
+    response.write(JSON.stringify(jsonObject["features"]))
     response.end();
   }
 
