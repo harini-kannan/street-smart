eec9740e594e31c3844ba4ce361dfaddbfdb359a
Harini Kannan <hkannan@mit.edu>
2014-08-02 18:13:30 -0700

    added bounding box

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ var Firebase = require('firebase')
 var myRootRef = new Firebase('streetsmartdb.firebaseIO.com/crimes')
 myRootRef.set("bebe")
 var jsonObject;
+var west = -122.415494;
+var south = 37.786564;
+var east = -122.405494;
+var north = 37.796564;
 
 var options = {
   host: 'sanfrancisco.crimespotting.org',
-  path: '/crime-data?format=json&count=2'
+  //path: '/crime-data?format=json&count=2'
+  //path: '/crime-data?format=json&count=2&bbox=-122.435494,37.796564,-122.395494,37.756564'
+  //path: '/crime-data?format=json&bbox=-122.28,37.7993,-122.2682,37.8077'
+  path: '/crime-data?format=json&bbox='+west+","+south+","+east+","+north
+  //-122.426723,37.769304
 };
 
 callback = function(response) {
